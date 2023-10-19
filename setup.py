9b2ed9c493d1b8f8f69dddd77d3d4bfa6151073e
Aditya Aditya <adityafulmali24@gmail.com>
2023-10-19 11:23:36 +0000

    Create pixel_art package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name='pixel_art',
+  version='0.1',
+  description='Package for drawing pixel art to theterminal.',
+  author='aditya',
+  packages=['pixel_art'],
+  install_requires=['colorama']
+)
